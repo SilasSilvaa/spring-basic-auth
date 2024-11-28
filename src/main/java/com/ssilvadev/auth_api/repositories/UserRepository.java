03b7995dfb9937f69dc5069d9b576d4a3a720438
SilasSilvaa <silas-oli@hotmail.com>
2024-11-27 23:04:42 -0300

    feat: add repository

---
+++ src/main/java/com/ssilvadev/auth_api/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ssilvadev.auth_api.repositories;
+
+import com.ssilvadev.auth_api.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+}
